12e938b5f6ff54e5701fa8062dacbd136eeed69b
Michael Craft <mcraft@peak15.org>
2013-09-07 01:39:27 -0400

    ooooooo yeeeeeaaa

--- giantindex.py
+++ giantindex.py
@@ -3,161 +3,151 @@
 
 import os
 import sys
-import time
 import contextlib
-import MySQLdb
 import ConfigParser
+import MySQLdb
+
 
 class DatabaseConnection(object):
-	def __init__(self, host, user, passwd, db):
-		self._db = MySQLdb.connect(
-				host=host, user=user, passwd=passwd, db=db)
-		#TODO: The connection should be closed.
-	
-	def __enter__(self):
-		return self
+    def __init__(self, host, user, passwd, db):
+        self._db = MySQLdb.connect(
+            host=host, user=user, passwd=passwd, db=db)
 
-	def __exit__(self, exc_type, exc_val, exc_tb):
-		self.close()
-	
-	def close(self):
-		"""Close the database connection."""
-		self._db.close()
-	
-	#TODO: consider 'with' syntax for cursor
-	def pathIsIndexed(self, path):
-		"""Return true if the path is in the database, false otherwise."""
-		
-		ret = True
-		with contextlib.closing(self._db.cursor()) as c:
-			c.execute('SELECT id FROM documents WHERE path = %s', (path,))
-			ret = c.fetchone() != None
+    def __enter__(self):
+        return self
 
-		return ret
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
 
-	def getDocumentPath(self, docID):
-		"""Return the path of the document with the given ID."""
-		c = self._db.cursor()
+    def close(self):
+        """Close the database connection."""
+        self._db.close()
 
-		c.execute('SELECT path FROM documents WHERE id = %s', (docID,))
-		path = c.fetchone()[0]
+    def pathIsIndexed(self, path):
+        """Return true if the path is in the database, false otherwise."""
 
-		c.close()
-		return path
+        with contextlib.closing(self._db.cursor()) as c:
+            c.execute('SELECT id FROM documents WHERE path = %s', (path,))
+            return c.fetchone() is not None
 
-	def addDocument(self, path):
-		"""
-		Create a blank database entry
-		for the document at the given path,
-		return a document id.
-		"""
-		c = self._db.cursor()
+    def getDocumentPath(self, docID):
+        """Return the path of the document with the given ID."""
 
-		c.execute('INSERT INTO documents (id, path, modified, duration, width, height, size) VALUES (NULL, %s, CURRENT_TIMESTAMP, NULL, NULL, NULL, 0)', (path,))
-		self._db.commit()
-		
-		c.execute('SELECT id FROM documents WHERE path = %s', (path,))
-		docID = c.fetchone()[0]
+        with contextlib.closing(self._db.cursor()) as c:
+            c.execute('SELECT path FROM documents WHERE id = %s', (docID,))
+            return c.fetchone()[0]
 
-		c.close()
-		return docID
+    def addDocument(self, path):
+        """
+        Create a blank database entry
+        for the document at the given path,
+        return a document id.
+        """
 
-	def updateAttributes(self, docID):
-		"""
-		Updates all attributes of and adds any applicable tags
-		to the document with the given ID.
-		"""
-		path = self.getDocumentPath(docID)
-		pathStat = os.stat(path)
-		
-		#TODO: skip checks if not modified since last scan
-		# Modified
-		modified = pathStat.st_mtime
-		#TODO: is this number in MySQL TIMESTAMP friendly format?
+        with contextlib.closing(self._db.cursor()) as c:
+            c.execute("""
+                    INSERT INTO documents (id, path, modified, duration, width, height, size)
+                    VALUES (NULL, %s, CURRENT_TIMESTAMP, NULL, NULL, NULL, 0)""", (path,))
+            self._db.commit()
 
-		# Size (in bytes)
-		size = pathStat.st_size
-		
-		# Add Applicable Tags
+            c.execute('SELECT id FROM documents WHERE path = %s', (path,))
+            return c.fetchone()[0]
 
-		
-		#TODO: Perhaps retain some info on file type to avoid checking tags
+    def updateAttributes(self, docID):
+        """
+        Updates all attributes of and adds any applicable tags
+        to the document with the given ID.
+        """
+        path = self.getDocumentPath(docID)
+        pathStat = os.stat(path)
 
-		# Duration
-		duration = None
-		if hasTag(docID, 'audio') or hasTag(docID, 'video'):
-			duration = None #get that duration!
+        #TODO: skip checks if not modified since last scan
+        # Modified
+        modified = pathStat.st_mtime
+        #TODO: is this number in MySQL TIMESTAMP friendly format?
 
-		width, height = None
-		if hasTag(docID, 'image') or hasTag(docID, 'video'):
-			
-			# Width
-			width = None #get that width!
-			
-			# Height
-			height = None #get height
+        # Size (in bytes)
+        size = pathStat.st_size
 
-		c = self._db.cursor()
+        # Add Applicable Tags
 
-		c.execute('UPDATE documents SET modified = %s, duration = %s, width = %s, height = %s, size = %s WHERE id = %s', (modified, duration, width, height, size, docID))
-		self._db.commit()
-		
-		c.close()
+
+        #TODO: Perhaps retain some info on file type to avoid checking tags
+
+        # Duration
+        duration = None
+        if hasTag(docID, 'audio') or hasTag(docID, 'video'):
+            duration = None #get that duration!
+
+        width, height = None
+        if hasTag(docID, 'image') or hasTag(docID, 'video'):
+
+            # Width
+            width = None #get that width!
+
+            # Height
+            height = None #get height
+
+        with contextlib.closing(self._db.cursor()) as c:
+            c.execute(
+                'UPDATE documents SET modified = %s, duration = %s, width = %s, height = %s, size = %s WHERE id = %s',
+                (modified, duration, width, height, size, docID))
+            self._db.commit()
+
 
 def scanNewFiles(root, db):
-	"""
-	Walk the given directory and
-	add any files to the database that are not already in it.
-	
-	Also loads up attributes, tags, and everything!
-	"""
-	
-	for directory, subdirs, files in os.walk(root):
-		for f in files:
-			if not db.pathIsIndexed(f):
-				doc = db.addDocument(f)
-				db.updateAttributes(doc)
-				print(os.path.join(directory, f))
+    """
+    Walk the given directory and
+    add any files to the database that are not already in it.
+
+    Also loads up attributes, tags, and everything!
+    """
+
+    for directory, subdirs, files in os.walk(root):
+        for f in files:
+            if not db.pathIsIndexed(f):
+                doc = db.addDocument(f)
+                db.updateAttributes(doc)
+                print(os.path.join(directory, f))
+
 
 def getSettings():
-	"""
-	Return a dict of settings and their values
-	from loading a settings file,
-	or exit the program if none is found.
-	"""
-	#TODO: Prompt the user? Cmd-line args?
+    """
+    Return a dict of settings and their values
+    from loading a settings file,
+    or exit the program if none is found.
+    """
+    #TODO: Prompt the user? Cmd-line args?
 
-	# Locate configuration file
-	cfgFile = None
-	cfg_name = 'giantindex.cfg'
-	if os.access(cfg_name, os.R_OK):
-		cfgFile = cfg_name
-	elif os.access(os.path.expanduser('~/.' + cfg_name), os.R_OK):
-		cfgFile = os.path.expanduser('~/.' + cfg_name)
-	elif os.access(os.path.join('/etc/', cfg_name), os.R_OK):
-		cfgFile = os.path.join('/etc/', cfg_name)
-	else:
-		print('Could not load ./giantindex.cfg, ~/.giantindex.cfg, or /etc/giantindex.cfg')
-		sys.exit(1)
-	
-	config = ConfigParser.RawConfigParser()
-	config.read(cfgFile)
-	
-	settings = {}
+    # Locate configuration file
+    cfg_name = 'giantindex.cfg'
+    if os.access(cfg_name, os.R_OK):
+        cfgFile = cfg_name
+    elif os.access(os.path.expanduser('~/.' + cfg_name), os.R_OK):
+        cfgFile = os.path.expanduser('~/.' + cfg_name)
+    elif os.access(os.path.join('/etc/', cfg_name), os.R_OK):
+        cfgFile = os.path.join('/etc/', cfg_name)
+    else:
+        print('Could not load ./giantindex.cfg, ~/.giantindex.cfg, or /etc/giantindex.cfg')
+        sys.exit(1)
 
-	for key in ('host', 'user', 'passwd', 'db'):
-		settings[key] = config.get('database', key)
-	for key in ('include', 'exclude'):
-		settings[key] = config.get('indexer', key)
-	
-	return settings
+    config = ConfigParser.RawConfigParser()
+    config.read(cfgFile)
+
+    settings = {}
+
+    for key in ('host', 'user', 'passwd', 'db'):
+        settings[key] = config.get('database', key)
+    for key in ('include', 'exclude'):
+        settings[key] = config.get('indexer', key)
+
+    return settings
+
 
 if __name__ == '__main__':
-	
-	cfg = getSettings()
-	
-	with DatabaseConnection(cfg['host'], cfg['user'], cfg['passwd'], cfg['db']) as db:
-		print(db.pathIsIndexed('/home/pghjgublic/test.txt'))
-		#db.updateAttributes(1)
 
+    cfg = getSettings()
 
+    with DatabaseConnection(cfg['host'], cfg['user'], cfg['passwd'], cfg['db']) as db:
+        print(db.pathIsIndexed('/home/pghjgublic/test.txt'))
+        #db.updateAttributes(1)
